ee9374f6c9f8db18c3a4c0a9dcb1fba59b4eaf80
NoxNode <dungeonsaboss@live.com>
2016-09-29 21:45:11 -0700

    added folders and moved some things into them

---
+++ astra_infinita/Objects/Items/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace astra_infinita.Objects {
+    public class Item : GameObject {
+        public Item() {
+
+        }
+
+        public Item(String name,Texture2D newTexture) {
+            objectName = name;
+            texture = newTexture;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch, Vector2 cameraPosition) { }
+
+        public override void Update(GameTime gameTime) { }
+    }
+}
